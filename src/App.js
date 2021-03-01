7a35ab4e926c15ae37eb7b8c3a2e25f0791980a4
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-01 21:27:27 +0100

    APP_ID and APP_KEY added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 function App() {
+
+  const APP_ID = "93efe5e7";
+  const APP_KEY = "184d313c90f01c78661e98dc4971cf1a";
+
   return (
     <div>
-      Hello
+      Hello React
     </div>
   );
 }
