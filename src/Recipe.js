2c0961849d031b777ab1e262e07dfaf1700a8899
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-04 20:01:41 +0100

    code formatted

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Recipe({title, calories, image, ingredients}) {
-    return (
-        <div>
-            <h1>{title}</h1>
-            <ol>{ingredients.map(ingredient => (
-                <li>{ingredient.text}</li>
-            ))}</ol>
-            <p>{calories}</p>
-            <img src={image} alt=""/>
-        </div>
-    )
+function Recipe({ title, calories, image, ingredients }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>{calories}</p>
+      <img src={image} alt="" />
+    </div>
+  );
 }
 
-export default Recipe
+export default Recipe;
