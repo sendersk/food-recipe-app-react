dc376d4c339b169ca7b664235344b0891d8a7430
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-04 20:00:40 +0100

    ingredients list added

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Recipe({title, calories, image}) {
+function Recipe({title, calories, image, ingredients}) {
     return (
         <div>
             <h1>{title}</h1>
+            <ol>{ingredients.map(ingredient => (
+                <li>{ingredient.text}</li>
+            ))}</ol>
             <p>{calories}</p>
             <img src={image} alt=""/>
         </div>
