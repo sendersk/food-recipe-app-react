ec9fdb9324f7fd39b358419ceb2e3d6f95b4f8f2
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-04 19:18:39 +0100

    getRecipes function created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const APP_ID = "93efe5e7";
   const APP_KEY = "184d313c90f01c78661e98dc4971cf1a";
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
