8a2367c8ed5d1f0c59d3c8f5ef570a634320f28a
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-04 18:47:47 +0100

    form added to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react"
+import React from "react";
 
 function App() {
-
   const APP_ID = "93efe5e7";
   const APP_KEY = "184d313c90f01c78661e98dc4971cf1a";
 
   return (
-    <div>
-      Hello React
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
